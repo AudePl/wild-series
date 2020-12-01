070638aa0fdd78388485a100bebf268fd83afe23
AudeP <aude.plana@gmail.com>
2020-12-01 11:33:07 +0100

    quete 08 : controller category done

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Category;
+use App\Entity\Program;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/categories", name="category_")
+ */
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        $categories = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findAll();
+        return $this->render('category/index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * @Route("/{category}", methods={"GET"}, name="show")
+     */
+    public function show(string $category):Response
+    {
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['name' => $category]);
+        if (!$category) {
+            throw $this->createNotFoundException(
+                'No category with name : '.$category.' found in category\'s table.'
+            );
+        }
+
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findBy(
+                ['category' => $category],
+                ['id' => 'DESC'],
+                3
+            );
+        if (!$programs){
+            throw $this->createNotFoundException(
+                'No program with category name : ' .$category. ' found in program\'s table.'
+            );
+        }
+        return $this->render('category/show.html.twig', [
+            'category' => $category,
+            'programs' => $programs,
+        ]);
+    }
+}
